51cb6f6e5036a083340cd0d0edc7630f4a938f70
jeff <jeffpeng2012@gmail.com>
2025-11-22 10:41:09 +0800

    use docker compose healthcheck

--- build/lite/src/install.sh
+++ build/lite/src/install.sh
@@ -11,19 +11,4 @@ sleep 2
 earnapp start
 sleep 2
 echo "Earnapp is running"
-earnapp run &
-
-fail_count=0
-while [ $fail_count -lt 3 ]; do
-    if ping -c 1 -W 2 8.8.8.8 >/dev/null 2>&1; then
-        fail_count=0
-    else
-        echo "網路連線失敗，重新嘗試..."
-        fail_count=$((fail_count + 1))
-    fi
-    if [ $fail_count -ge 3 ]; then
-        echo "網路連線失敗超過三次，結束容器"
-        exit 1
-    fi
-    sleep 30
-done
+earnapp run
